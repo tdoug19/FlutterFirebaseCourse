0c9f90905e813850e91deedb5f8775c01e88e56f
Trevor Douglas <tdoug19@gmail.com>
2021-09-06 12:28:27 -0600

    Updated row feature

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -12,25 +12,25 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
-                height: 100.0,
-                width: 150.0,
+
+                width: 100.0,
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
+
               Container(
-                height: 100.0,
-                width: 150.0,
+                width: 100.0,
+                height: 50.0,
                 color: Colors.red,
                 child: Text('Container 2'),
               ),
+
               Container(
-                height: 100.0,
-                width: 150.0,
+                width: 100.0,
                 color: Colors.blue,
                 child: Text('Container 3'),
               ),
