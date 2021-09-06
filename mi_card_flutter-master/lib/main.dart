08448274a0254f031dae9a247c0153e5aee52b5c
Trevor Douglas <tdoug19@gmail.com>
2021-09-06 07:44:18 -0600

    containers

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -9,8 +9,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context ) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellow,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 150.0,
+            margin: EdgeInsets.only(left: 10.0),
+            color: Colors.white,
+            child: Text('hello there'),
+          ),
+        ),
       ),
     );
   }
