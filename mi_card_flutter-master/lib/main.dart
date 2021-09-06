fd8d2a4c039f7990672e837d3bb28db78b09d211
Trevor Douglas <tdoug19@gmail.com>
2021-09-06 08:07:03 -0600

    Column Containers and row containers

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -11,12 +11,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 150.0,
-            margin: EdgeInsets.only(left: 10.0),
-            color: Colors.white,
-            child: Text('hello there'),
+          child: Column(
+            children: [
+              Container(
+                height: 100.0,
+                width: 150.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                width: 150.0,
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 150.0,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
