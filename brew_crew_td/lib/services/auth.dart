ba14b520c403e8ec621083b86e088a3f9df9caf7
Trevor Douglas <tdoug19@gmail.com>
2021-09-16 10:53:36 -0600

    Finished vid 4.  Ready to authenticate

---
+++ brew_crew_td/lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;  //_ means private  I think
+
+  // sign in anon
+
+  Future signInAnon() async {
+    try{
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+
+    } catch (e) {
+
+    }
+  }
+
+  // sign in with email and pw
+  // register with email and pw
+  // sign out as well
+
+
+}
