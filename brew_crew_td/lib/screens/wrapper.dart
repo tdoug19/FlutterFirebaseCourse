c9fe351f9bfe28ae410dffa262c5a3ca04a62c8f
Trevor Douglas <tdoug19@gmail.com>
2021-09-16 10:33:47 -0600

    Finished video 3 creating the app structure

---
+++ brew_crew_td/lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:brew_crew_td/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // return Home or Authenticate widget
+    return Home();
+  }
+}
