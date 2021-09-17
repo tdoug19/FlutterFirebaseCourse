0f57b12cfe84035dd4025a4fd1821d449e0630f0
Trevor Douglas <tdoug19@gmail.com>
2021-09-17 11:10:40 -0600

    Finished logging in ID in https://www.youtube.com/watch?v=LkpPEYuqbIY

--- brew_crew_td/lib/screens/authenticate/sign_in.dart
+++ brew_crew_td/lib/screens/authenticate/sign_in.dart
@@ -32,7 +32,7 @@ class _SignInState extends State<SignIn> {
             }
             else {
               print('signed in');
-              print(result);
+              print(result.uid);
             }
           }
         )
--- brew_crew_td/lib/services/auth.dart
+++ brew_crew_td/lib/services/auth.dart
@@ -1,16 +1,25 @@
+import 'package:brew_crew_td/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;  //_ means private  I think
 
+
+  //create User object based on FirebaseUser
+  UserFirebase _userFromFirebaseUser(User user) {
+
+    return UserFirebase(uid: user.uid);
+  }
   // sign in anon
 
   Future signInAnon() async {
+
     try{
       UserCredential result = await _auth.signInAnonymously();
-      User? user = result.user;
-      return user;
+      User user = result.user!;
+
+      return _userFromFirebaseUser(user);
 
     } catch (e) {
       print(e.toString());
