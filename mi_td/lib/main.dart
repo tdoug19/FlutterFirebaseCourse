b0c4ddffbeec24c220dd9c6053526ffe0df38471
Trevor Douglas <trevordouglas@Trevors-MacBook-Air.local>
2022-04-14 15:59:59 -0600

    finished mi card

--- mi_td/lib/main.dart
+++ mi_td/lib/main.dart
@@ -8,106 +8,92 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context ) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget> [
-              CircleAvatar(
-                //Image to go here
-                radius: 50.0,
-              ),
-              SizedBox(
-                height: 10.0
-              ),
-              Text(
-                'Trevor Douglas',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontFamily: 'Pacifico',
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  //Image to go here
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/TD.png'),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Trevor Douglas',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.tealAccent,
+                    letterSpacing: 2.0,
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                    height: 10.0,
+                    width: 200.0,
+                    child: Divider(
+                      color: Colors.tealAccent,
+                      height: 20,
+                      thickness: 3,
+                    )),
+                Card(
                   color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 10.0
-              ),
-              Text(
-                'SOFTWARE ENGINEER',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.tealAccent,
-                  letterSpacing: 2.0,
-                  fontFamily: 'SourceSansPro',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 10.0
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.phone,
-                        size: 50.0,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        "306.351.0715",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontFamily: 'SourceSansPro',
-                          letterSpacing: 1.5,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          size: 50.0,
                           color: Colors.teal,
-                        )
-                      ),
-                    ],
-                  ),
+                        ),
+                        title: Text("306.351.0715",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontFamily: 'SourceSansPro',
+                              letterSpacing: 1.5,
+                              color: Colors.teal,
+                            )),
+                      )),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          size: 50.0,
+                          color: Colors.teal,
+                        ),
+                        title: Text("trevor.douglas@uregina.ca",
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontFamily: 'SourceSansPro',
+                              letterSpacing: 1.0,
+                              color: Colors.teal,
+                            )),
+                      )),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.email,
-                        size: 50.0,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                          "tdoug19gmail.com",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'SourceSansPro',
-                            letterSpacing: 1.5,
-                            color: Colors.teal,
-                          )
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -115,3 +101,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Row(
+                  //   children: <Widget>[
+                  //     Icon(
+                  //       Icons.email,
+                  //       size: 50.0,
+                  //       color: Colors.teal,
+                  //     ),
+                  //     SizedBox(
+                  //       width: 20.0,
+                  //     ),
+                  //     Text("tdoug19gmail.com",
+                  //         style: TextStyle(
+                  //           fontSize: 20.0,
+                  //           fontFamily: 'SourceSansPro',
+                  //           letterSpacing: 1.5,
+                  //           color: Colors.teal,
+                  //         )),
+                  //   ],
+                  // ),
